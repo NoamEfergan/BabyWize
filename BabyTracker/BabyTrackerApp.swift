67a6bbc6463c08a1c351912084a2b23b80a4ac58
Noam Efergan <Noam.Efergan@Gamesys.co.uk>
2022-07-17 10:57:15 +0100

    Initial Commit

---
+++ BabyTracker/BabyTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BabyTrackerApp.swift
+//  BabyTracker
+//
+//  Created by Noam Efergan on 17/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct BabyTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
