c12541bdffcd62f8cd549a405244d7b97291b76e
Noam Efergan <efergann@gmail.com>
2022-09-17 12:34:39 +0200

    Add swiftformatter

--- BabyWize/BabyWizeApp.swift
+++ BabyWize/BabyWizeApp.swift
@@ -5,14 +5,27 @@
 //  Created by Noam Efergan on 17/07/2022.
 //
 
+import FirebaseCore
 import SwiftUI
 import Swinject
 
+// class AppDelegate: NSObject, UIApplicationDelegate {
+//  func application(_ application: UIApplication,
+//                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+//    FirebaseApp.configure()
+//
+//    return true
+//  }
+// }
+
 @main
 struct BabyWizeApp: App {
+//    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     init() {
         let mainContainer = ContainerBuilder.buildMainContainer()
         Resolver.shared.setDependencyContainer(mainContainer)
+        FirebaseApp.configure()
     }
 
     @Environment(\.scenePhase) var scenePhase
