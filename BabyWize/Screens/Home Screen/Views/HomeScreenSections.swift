92e0f690f248bff9f5cf6643d2e3a16a6aacb5f7
Noam Efergan <efergann@gmail.com>
2022-10-23 12:33:23 +0100

    Clean up charts section

--- BabyWize/Screens/Home Screen/Views/HomeScreenSections.swift
+++ BabyWize/Screens/Home Screen/Views/HomeScreenSections.swift
@@ -9,54 +9,62 @@ import Charts
 import SwiftUI
 
 // MARK: - HomeScreenSections
-
 struct HomeScreenSections: View {
     @InjectedObject private var dataManager: BabyDataManager
+    private let nothingYetTitle = "Nothing to show yet!"
 
     var body: some View {
-        let feedInfoTitle =
-            "feed info \(dataManager.feedData.isEmpty ? "" : "(last \(dataManager.feedData.count >= 6 ? 6 : dataManager.feedData.count))")"
-
-        let sleepInfoTitle =
-            "sleep info \(dataManager.sleepData.isEmpty ? "" : "(last \(dataManager.sleepData.count >= 3 ? 3 : dataManager.sleepData.count))")"
-
         let feedData = dataManager.feedData.getUpTo(limit: 6)
         let sleepData = dataManager.sleepData.getUpTo(limit: 3)
 
         VStack {
-            if dataManager.feedData.isEmpty {
-                Text("Nothing to show yet, add a feed!")
-            } else {
-                Chart(feedData) { feed in
-                    LineMark(x: .value("Time", feed.date.formatted(date: .omitted, time: .shortened)),
-                             y: .value("Amount", feed.amount))
-                        .foregroundStyle(Color.blue.gradient)
-                }
-                .frame(height: 200)
+            Text(feedInfoTitle)
+            Chart(feedData) { feed in
+                LineMark(x: .value("Time", feed.date.formatted(date: .omitted, time: .shortened)),
+                         y: .value("Amount", feed.amount))
+                    .foregroundStyle(Color.blue.gradient)
+            }
+            .frame(height: 200)
+            if !dataManager.feedData.isEmpty {
                 NavigationLink("More Info", value: InfoScreens.feed)
             }
         }
         VStack {
-            if dataManager.sleepData.isEmpty {
-                Text("Nothing to show yet, add a sleep!")
-            } else {
-                Chart(sleepData) { sleep in
-                    let dateValue = sleep.date.formatted(date: .omitted, time: .shortened)
-                    let amountValue = sleep.duration.convertToTimeInterval().displayableString
-                    BarMark(x: .value("Time", "\(dateValue)\n \(amountValue)"),
-                            y: .value("Amount", sleep.duration.convertToTimeInterval()))
-                        .foregroundStyle(Color.red.gradient)
-                }
-                .chartYAxis(.hidden)
-                .frame(height: 200)
+            Chart(sleepData) { sleep in
+                let dateValue = sleep.date.formatted(date: .omitted, time: .shortened)
+                let amountValue = sleep.duration.convertToTimeInterval().displayableString
+                BarMark(x: .value("Time", "\(dateValue)\n \(amountValue)"),
+                        y: .value("Amount", sleep.duration.convertToTimeInterval()))
+                    .foregroundStyle(Color.red.gradient)
+            }
+            .chartYAxis(.hidden)
+            .frame(height: 200)
+            if !dataManager.sleepData.isEmpty {
                 NavigationLink("More Info", value: InfoScreens.sleep)
             }
+        }
+    }
+
+    private var feedInfoTitle: String {
+        if dataManager.feedData.isEmpty {
+            return nothingYetTitle
+        } else {
+            let feedCount = dataManager.feedData.count >= 6 ? 6 : dataManager.feedData.count
+            return "Feed info (last \(feedCount)"
+        }
+    }
+
+    private var sleepInfoTitle: String {
+        if dataManager.sleepData.isEmpty {
+            return nothingYetTitle
+        } else {
+            let feedCount = dataManager.sleepData.count >= 3 ? 3 : dataManager.sleepData.count
+            return "Sleep info (last \(feedCount)"
         }
     }
 }
 
 // MARK: - HomeScreenSections_Previews
-
 struct HomeScreenSections_Previews: PreviewProvider {
     static var previews: some View {
         ScrollView {
