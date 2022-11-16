e766359a3350210dd6579c1a14e86b499edd4f64
Noam Efergan <efergann@gmail.com>
2022-11-16 15:35:18 +0000

    Started work to fetch all feeds from remote

--- BabyWize/Managers/Firebase/FirebaseManager.swift
+++ BabyWize/Managers/Firebase/FirebaseManager.swift
@@ -9,31 +9,37 @@ import FirebaseCore
 import FirebaseFirestore
 import Foundation
 
-struct FirebaseManager {
+// MARK: - FirebaseManager
+final class FirebaseManager {
     @InjectedObject private var defaultsManager: UserDefaultManager
-    let db = Firestore.firestore()
-    let authVM = AuthViewModel()
+    private let db = Firestore.firestore()
+    private let authVM = AuthViewModel()
+    private var userID: String?
 
     init() {
+        userID = defaultsManager.userID
         loginIfPossible()
+        Task {
+            await getAllFeeds()
+        }
     }
 
     func addFeed(_ item: Feed) {
-        guard let id = defaultsManager.userID else {
+        guard let userID else {
             return
         }
         let feedDTO: [String: Any] = [
-            "date": item.date,
-            "amount": item.amount,
-            "note": item.note ?? "",
-            "solidOrLiquid": item.solidOrLiquid.rawValue
+            FBKeys.kDate: item.date,
+            FBKeys.kAmount: item.amount,
+            FBKeys.kNote: item.note ?? "",
+            FBKeys.kSolidLiquid: item.solidOrLiquid.rawValue
         ]
         // Add a new document with a generated ID
         var ref: DocumentReference?
         ref = db
-            .collection("users")
-            .document(id)
-            .collection("feeds")
+            .collection(FBKeys.kUsers)
+            .document(userID)
+            .collection(FBKeys.kFeeds)
             .addDocument(data: [item.id: feedDTO]) { err in
                 if let err {
                     print("Error adding document: \(err)")
@@ -43,15 +49,57 @@ struct FirebaseManager {
             }
     }
 
+    func getAllFeeds() async {
+        guard let userID,
+              let remoteFeedSnapshot = try? await db
+              .collection(FBKeys.kUsers)
+              .document(userID)
+              .collection(FBKeys.kFeeds)
+              .getDocuments() else {
+            return
+        }
+        let feeds = remoteFeedSnapshot.mapToDomainFeed()
+        print(feeds)
+    }
+
     private func loginIfPossible() {
         Task {
             if defaultsManager.hasAccount {
                 let credentials = try? KeychainManager.fetchCredentials()
-                defaultsManager.userID = await authVM.login(email: credentials?.email ?? "",
-                                                            password: credentials?.password ?? "")
+                let id = await authVM.login(email: credentials?.email ?? "",
+                                            password: credentials?.password ?? "")
+                defaultsManager.userID = id
+                self.userID = id
             } else {
-                defaultsManager.userID = await authVM.anonymousLogin()
+                let id = await authVM.anonymousLogin()
+                defaultsManager.userID = id
+                self.userID = id
             }
         }
     }
 }
+
+extension QueryDocumentSnapshot {
+    func mapToFeed() -> Feed? {
+        guard let data = data().values.first as? [String: Any],
+              let amount = data[FBKeys.kAmount] as? Double,
+              let note = data[FBKeys.kNote] as? String,
+              let solidOrLiquid = data[FBKeys.kSolidLiquid] as? String,
+              let timeStamp = data[FBKeys.kDate] as? Timestamp
+        else {
+            return nil
+        }
+        return .init(id: documentID,
+                     date: Date(timeIntervalSince1970: Double(timeStamp.seconds)),
+                     amount: amount,
+                     note: note.isEmpty ? nil : note,
+                     solidOrLiquid: .init(rawValue: solidOrLiquid) ?? .liquid)
+    }
+}
+
+extension QuerySnapshot {
+    func mapToDomainFeed() -> [Feed] {
+        documents.compactMap({ $0.mapToFeed() })
+    }
+}
+
---
+++ BabyWize/Models/Models/FBKeys.swift
@@ -0,0 +1,20 @@
+//
+//  FBKeys.swift
+//  BabyWize
+//
+//  Created by Noam Efergan on 16/11/2022.
+//
+
+import Foundation
+
+struct FBKeys {
+    static let kUsers = "users"
+    static let kFeeds = "feeds"
+    static let kSleeps = "sleeps"
+    static let kChanges = "changes"
+    
+    static let kAmount = "amount"
+    static let kDate = "date"
+    static let kNote = "note"
+    static let kSolidLiquid = "solidOrLiquid"
+}
