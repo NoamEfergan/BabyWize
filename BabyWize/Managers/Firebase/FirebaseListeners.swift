fe9f03609c7194c550bdec8e39cfe5f8a9e4b7df
Noam Efergan <efergann@gmail.com>
2022-12-17 20:27:13 +0000

    Update sleeps and nappy changes from remote

--- BabyWize/Managers/Firebase/FirebaseListeners.swift
+++ BabyWize/Managers/Firebase/FirebaseListeners.swift
@@ -48,14 +48,29 @@ extension FirebaseManager {
         guard let userID = defaultsManager.userID else {
             return
         }
-        addFeedsListener(userID: userID)
+        // Filtering out one of the feeds as it doesn't matter which one, and this will ensure no double listeners are
+        // added
+        EntryType
+            .allCases
+            .filter { $0 != .solidFeed }
+            .forEach { self.addEntryListenerListener(userID: userID, for: $0) }
     }
 
-    private func addFeedsListener(userID: String) {
+    private func addEntryListenerListener(userID: String, for entryType: EntryType) {
+        var collectionName: String {
+            switch entryType {
+            case .liquidFeed, .solidFeed:
+                return FBKeys.kFeeds
+            case .sleep:
+                return FBKeys.kSleeps
+            case .nappy:
+                return FBKeys.kChanges
+            }
+        }
         db
             .collection(FBKeys.kUsers)
             .document(userID)
-            .collection(FBKeys.kFeeds)
+            .collection(collectionName)
             .addSnapshotListener { [weak self] snapshot, error in
                 guard let self else {
                     return
@@ -63,7 +78,14 @@ extension FirebaseManager {
                 if let error {
                     print("Failed with error: \(error.localizedDescription)")
                 }
-                snapshot?.documentChanges.forEach(self.handleFeedDocumentChange)
+                switch entryType {
+                case .liquidFeed, .solidFeed:
+                    snapshot?.documentChanges.forEach(self.handleFeedDocumentChange)
+                case .sleep:
+                    snapshot?.documentChanges.forEach(self.handleSleepDocumentChange)
+                case .nappy:
+                    snapshot?.documentChanges.forEach(self.handleNappyDocumentChange)
+                }
             }
     }
 
@@ -72,19 +94,59 @@ extension FirebaseManager {
         case .added:
             if let feed = change.document.mapToFeed() {
                 dataManager?.addFeed(feed, updateRemote: false)
+            } else {
+                print("Failed to add feed from remote notification")
             }
         case .modified:
             if let feed = change.document.mapToFeed(),
                let index = dataManager?.feedData.firstIndex(where: { $0.id == feed.id }) {
                 dataManager?.updateFeed(feed, index: index, updateRemote: false)
+            } else {
+                print("Failed to modify feed from remote notification")
             }
         case .removed:
-            print(change.document.data())
-            if let feed = change.document.mapToFeed(),
-               let indices = dataManager?.feedData.filter({ $0.id == feed.id }).indices.compactMap({ Int($0) }) {
-                let indexSet = IndexSet(indices)
-                dataManager?.removeFeed(at: indexSet)
+            print("Feed removed in remote")
+        }
+    }
+
+    private func handleSleepDocumentChange(_ change: DocumentChange) {
+        switch change.type {
+        case .added:
+            if let sleep = change.document.mapToSleep() {
+                dataManager?.addSleep(sleep, updateRemote: false)
+            } else {
+                print("Failed to add sleep from remote notification")
             }
+        case .modified:
+            if let sleep = change.document.mapToSleep(),
+               let index = dataManager?.sleepData.firstIndex(where: { $0.id == sleep.id }) {
+                dataManager?.updateSleep(sleep, index: index, updateRemote: false)
+            } else {
+                print("Failed to modify sleep from remote notification")
+            }
+        case .removed:
+            print("Sleep removed in remote")
+        }
+    }
+
+
+    private func handleNappyDocumentChange(_ change: DocumentChange) {
+        switch change.type {
+        case .added:
+            if let nappyChange = change.document.mapToChange() {
+                dataManager?.addNappyChange(nappyChange, updateRemote: false)
+            } else {
+                print("Failed to add nappy change from remote notification")
+            }
+        case .modified:
+            if let nappyChange = change.document.mapToChange(),
+               let index = dataManager?.nappyData.firstIndex(where: { $0.id == nappyChange.id }) {
+                dataManager?.updateChange(nappyChange, index: index, updateRemote: false)
+            } else {
+                print("Failed to modify nappy change from remote notification")
+            }
+        case .removed:
+            print("Nappy change removed in remote")
         }
     }
 }
