83ea1e8a09c99fffc667e0513abc0b2bdd666630
Noam Efergan <efergann@gmail.com>
2022-12-19 14:03:54 +0000

    Add CRUD tests for baby data manager

--- BabyWizeTests/BabyDataManagerTests.swift
+++ BabyWizeTests/BabyDataManagerTests.swift
@@ -30,10 +30,88 @@ final class BabyDataManagerTests: XCTestCase {
         XCTAssertTrue(manager.nappyData.isEmpty)
     }
 
-    func testAddingFeed() throws {
+    func testCRUDfeed() throws {
+        // Create
         let feed = mockFeeds.randomElement()
         let safeFeed = try XCTUnwrap(feed)
         manager.addFeed(safeFeed)
         XCTAssertEqual(manager.feedData.count, 1)
+
+        // Update
+
+        var firstFeed = try XCTUnwrap(manager.feedData.first)
+        firstFeed.amount = 12.0
+        let index = try XCTUnwrap(manager.feedData.firstIndex(where: { $0.id == firstFeed.id }))
+        manager.updateFeed(firstFeed, index: index)
+        XCTAssertEqual(manager.feedData.first?.amount, 12.0)
+
+        // Remove
+        let indexSet = IndexSet(manager.feedData.indices.compactMap { Int($0) })
+        manager.removeFeed(at: indexSet)
+        XCTAssertTrue(manager.feedData.isEmpty)
+    }
+
+    func testCRUDsleep() throws {
+        // Create
+        let sleep = mockSleeps.randomElement()
+        let safeSleep = try XCTUnwrap(sleep)
+        manager.addSleep(safeSleep)
+        XCTAssertEqual(manager.sleepData.count, 1)
+
+        // Update
+        let newDate: Date = .now
+        let firstSleep = try XCTUnwrap(manager.sleepData.first)
+        let newSleep = Sleep(id: firstSleep.id, date: firstSleep.date, start: newDate, end: firstSleep.end)
+        let index = try XCTUnwrap(manager.sleepData.firstIndex(where: { $0.id == firstSleep.id }))
+        manager.updateSleep(newSleep, index: index)
+        XCTAssertEqual(manager.sleepData.first?.start, newDate)
+
+        // Remove
+        let indexSet = IndexSet(manager.sleepData.indices.compactMap { Int($0) })
+        manager.removeSleep(at: indexSet)
+        XCTAssertTrue(manager.sleepData.isEmpty)
+    }
+
+    func testCRUDnappy() throws {
+        // Create
+        let change = NappyChange(id: UUID().uuidString, dateTime: .now, wetOrSoiled: .wet)
+        manager.addNappyChange(change)
+        XCTAssertEqual(manager.nappyData.count, 1)
+
+        // Update
+        let firstChange = try XCTUnwrap(manager.nappyData.first)
+        let newChange = NappyChange(id: firstChange.id, dateTime: firstChange.dateTime, wetOrSoiled: .soiled)
+        let index = try XCTUnwrap(manager.nappyData.firstIndex(where: { $0.id == firstChange.id }))
+        manager.updateChange(newChange, index: index)
+        XCTAssertEqual(manager.nappyData.first?.wetOrSoiled, .soiled)
+
+        // Remove
+        let indexSet = IndexSet(manager.nappyData.indices.compactMap { Int($0) })
+        manager.removeChange(at: indexSet)
+        XCTAssertTrue(manager.nappyData.isEmpty)
+    }
+
+    func testVerifyUniqueness() {
+        let id = UUID().uuidString
+        let firstFeed = Feed(id: id, date: .now, amount: 12, note: nil, solidOrLiquid: .liquid)
+        let secondFeed = Feed(id: UUID().uuidString, date: .now, amount: 12, note: nil, solidOrLiquid: .liquid)
+        let thirdFeed = Feed(id: id, date: .now, amount: 12, note: nil, solidOrLiquid: .liquid)
+
+        manager.mergeFeedsWithRemote([firstFeed,secondFeed,thirdFeed])
+        XCTAssertEqual(manager.feedData.count, 2)
+
+        let firstSleep = Sleep(id: id, date: .now, start: .now, end: .now)
+        let secondSleep = Sleep(id: UUID().uuidString, date: .now, start: .now, end: .now)
+        let thirdSleep = Sleep(id: id, date: .now, start: .now, end: .now)
+
+        manager.mergeSleepsWithRemote([firstSleep, secondSleep, thirdSleep])
+        XCTAssertEqual(manager.sleepData.count, 2)
+
+        let firstChange = NappyChange(id: id, dateTime: .now, wetOrSoiled: .soiled)
+        let secondChange = NappyChange(id: UUID().uuidString, dateTime: .now, wetOrSoiled: .soiled)
+        let thirdChange = NappyChange(id: id, dateTime: .now, wetOrSoiled: .soiled)
+
+        manager.mergeChangesWithRemote([firstChange, secondChange, thirdChange])
+        XCTAssertEqual(manager.nappyData.count, 2)
     }
 }
